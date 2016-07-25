7fd89f7fac394a268539abc8db8d0daac04a38fb
sisi <simdorvil@hotmail.com>
2016-07-25 20:54:31 +0000

    alooooo

--- server.js
+++ server.js
@@ -9,10 +9,38 @@ var app = express();
 
 
 
-app.get('/hello', function(req, res) {
-    var input = req.query.name;
-    res.send(`<h1>Hey ${input}</h1>`);
+// app.get('/hello', function(req, res) {
+//     var input = req.query.name; // query says take the object after
+//     res.send(`<h1>Hey ${input}</h1>`);
+// });
+
+app.get('/calculator/:operation', function(req, res) {
+    var op = req.params.operation;
+    var numOne = req.query.num1;
+    var numTwo = req.query.num2;
+    var object = {'operand': op, 'firstOperand': numOne, 'secondOperand': numTwo, 'solution': null};
+    var code = 200;
+
+    switch(op) {
+        case 'add':
+            object.solution = numOne+numTwo;
+            break;
+        case 'sub':
+            object.solution = numOne-numTwo;
+            break;
+        case 'mult':
+            object.solution = numOne*numTwo;
+            break;
+        case 'div':
+            object.solution = numOne/numTwo;
+            break;
+        default:
+            code = 400;
+            break;
+    }
+    res.status(code).send(object);
 });
+
 
 
 
