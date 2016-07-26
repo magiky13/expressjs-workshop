c89be9e8528cc3483d802a90d0006e2254aab174
sisi <simdorvil@hotmail.com>
2016-07-26 00:52:46 +0000

    everything

--- server.js
+++ server.js
@@ -1,18 +1,29 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'magiky13', 
+  password : '',
+  database: 'reddit'
+});
 
 
-// app.get('/hello', function(req, res) {
-//     res.send('<h1>Hey Mom</h1>');
-// });
+var reddit = require('./reddit.js');
+var redditAPI = reddit(connection);
+
+app.get('/hello', function(req, res) {
+    res.send('<h1>Archichic</h1>');
+});
 
 
-
-
-// app.get('/hello', function(req, res) {
-//     var input = req.query.name; // query says take the object after
-//     res.send(`<h1>Hey ${input}</h1>`);
-// });
+app.get('/hello', function(req, res) {
+    var input = req.query.name; // query says take the object after
+    res.send(`<h1>Hey ${input}</h1>`);
+});
 
 app.get('/calculator/:operation', function(req, res) {
     var op = req.params.operation;
@@ -40,6 +51,76 @@ app.get('/calculator/:operation', function(req, res) {
     }
     res.status(code).send(object);
 });
+
+
+app.get('/posts', function(req, res){
+     var num = parseInt(req.query.id);
+     redditAPI.getFivePost(num, {}, function(err, posts){
+      function createLi(post){
+         return `
+         <li>
+          <p>
+             ${post.title}
+          </p>
+         </li>
+         <ul><li>
+          <p>
+             ${post.url}
+          </p>
+         </li></ul>
+         `;}
+    
+    console.log(posts);
+         var html = `
+         <div id="contents">
+         <h1>List of contents</h1>
+         <ul class="contents-list">
+          ${posts.map(function(post){
+             return createLi(post);
+          }).join("")}
+         </ul>
+         </div>`;
+         res.send(html);
+    });
+});
+
+
+app.get('/createContent', function(req, res) {
+    
+    var htmlForm =
+   
+    `<form action="/createContent" method="POST"> 
+        
+        <div>
+            <input type="text" name="url" placeholder="Enter a URL to content">
+        
+        </div>
+        
+        <div>
+            <input type="text" name="title" placeholder="Enter the title of your content">
+        
+        </div>
+        
+            <button type="submit">Create!</button>
+            
+        </form>`;
+        
+    res.send(htmlForm);
+    
+});
+
+app.post('/createContent', function (req, res) {
+    console.log(req.body);
+    redditAPI.createPost( {
+        title: req.body.title,
+        url : req.body.url,
+        userId: 1
+    }, 
+    function(err, post){
+        res.send(post);
+    });
+});
+
 
 
 
